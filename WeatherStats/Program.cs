e63cb11f8c2217672f2f407ca6dfc6f7d8d62700
Peter Villani <peter.villani.ext@siemens.com>
2017-01-11 14:33:09 +0100

    Initial checkin

---
+++ WeatherStats/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using EF6CodeFirstTutorials;
+using WeatherStats.Model;
+
+namespace WeatherStats
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("*** Starting Weather Stats data acquisition ***");
+            Console.WriteLine("");
+
+            var hostName =  System.Configuration.ConfigurationManager.AppSettings["DatabaseHostName"];
+            var databaseName = System.Configuration.ConfigurationManager.AppSettings["DatabaseName"];
+            var userId = System.Configuration.ConfigurationManager.AppSettings["userId"];
+            var password= System.Configuration.ConfigurationManager.AppSettings["password"];
+
+            //TODO add some check that the values are populated in the config file
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = hostName;
+            builder.InitialCatalog = databaseName;
+            builder.UserID = userId;
+            builder.Password = password;
+            builder.IntegratedSecurity = false;
+            using (var context = new WeatherStatsContext(builder.ConnectionString))
+            
+            {
+                context.Students.Add(new Student() { StudentName = "New Student using sp" });
+                context.SaveChanges();
+            }
+
+            WeatherStatsPoller wsp = new WeatherStatsPoller("Aarhus","DK");
+            Task.Run(()=>wsp.PollDataFromWeb().ConfigureAwait(false));
+
+            while (wsp.IsDone == false)
+            {
+                
+            }
+
+            Console.WriteLine($"I think the temperature in {wsp.City},{wsp.Country} is {wsp.Temperature} C");
+            Console.WriteLine("*** Done with Weather Stats data acquisition ***");
+        }
+    }
+}
+
+
+
---
+++ WeatherStats/WeatherStatsPoller.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace EF6CodeFirstTutorials
+{
+    public class WeatherStatsPoller
+    {
+        private double temperature;
+
+        private string city;
+
+        private string country;
+
+        public WeatherStatsPoller(string city, string country)
+        {
+            this.city = city;
+            this.country = country;
+        }
+
+        public bool IsDone = false;
+        
+        public string City => this.city;
+        
+        public string Country => this.country;
+
+        public double Temperature
+        {
+            get
+            {
+                return temperature;
+            }
+            set
+            {
+                temperature = value;
+                this.IsDone = true;
+            }
+        }
+
+        public async Task PollDataFromWeb()
+        {
+            try
+            {
+        
+                //TODO use nuget package and NewtonSoft to parse json properly
+                var weburl = GetUrlForCity(city, country);
+                var json=  new WebClient().DownloadStringTaskAsync(new Uri(weburl));
+                await json;
+                var splitToString = json.Result.Split(':');
+                for(int i = 0 ; i!= splitToString.Length;i++)
+                {
+                    if (splitToString[i].Contains("temp") && i < splitToString.Length)
+                    {
+                        var subStr = splitToString[i + 1].Split(',')[0];
+                        double temp = double.Parse(subStr.Replace('.',',')) - 273.15;
+                        Temperature = temp;
+                        break;
+                    }
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private string GetUrlForCity(string city, string country)
+        {
+            //Note the API key is unique and cannot be changed.
+            return $"http://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid=f20cf3b8e8ea1eb76ca2f9e8c4957bb1";
+        }
+    }
+}
